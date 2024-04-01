764c16252a76908400cb267fb52d823d157eb616
Zenoviy Fil <98259328+zenoviyfil@users.noreply.github.com>
2024-04-01 11:01:32 +0200

    contactsOps

--- src/redux/contactsOps.js
+++ src/redux/contactsOps.js
@@ -3,6 +3,35 @@ import axios from "axios";
 
 axios.defaults.baseURL = "https://660a6ff80f324a9a28850459.mockapi.io";
 
-export const fetchContacts = createAsyncThunk()
-export const addContacts = createAsyncThunk()
-export const deleteContacts = createAsyncThunk()
+export const fetchContacts = createAsyncThunk(
+    "contacts/fetchAll", async (_, thunkApi) => {
+        try {
+            const res = await axios.get("/contacts")
+            return res.data
+        } catch (error) {
+            return thunkApi.rejectWithValue(error.message)
+        }
+    }
+)
+
+export const addContacts = createAsyncThunk(
+    "tasks/addContact", async (values, thunkApi) => {
+        try {
+            const res = await axios.post("/contacts", {name:values.name, phone: values.phone})
+            return res.data
+        } catch (error) {
+            return thunkApi.rejectWithValue(error.message)
+        }
+    }
+)
+
+export const deleteContacts = createAsyncThunk(
+    "tasks/deleteContact", async (contactId, thunkApi) => {
+        try {
+            const res = await axios.delete(`/contacts/${contactId}`)
+            return res.data
+        } catch (error) {
+            return thunkApi.rejectWithValue(error.message)
+        }
+    }
+)
